32b606fb586ebbfd67c515fc938100eee23bf5ab
Karnsaty1 <karnsaty69@gmail.com>
2026-05-11 10:32:57 +0530

    Added

--- src/modules/auth/auth.utils.ts
+++ src/modules/auth/auth.utils.ts
@@ -6,7 +6,6 @@ import {
   type SignOptions,
 } from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import type { ObjectId } from "mongodb";
 
 interface TokenUserLike {
   _id: { toString(): string } | string;
@@ -184,11 +183,3 @@ export {
   hashPassword,
   comparePassword,
 };
-
-export function createTokens(userId: ObjectId) {
-  return buildTokenPair({
-    _id: userId,
-    email: "",
-    role: "user",
-  });
-}
